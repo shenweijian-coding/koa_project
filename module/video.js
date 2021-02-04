91347ccaf2c2c2c88747eca3ec68b93c68db7d9a
shenweijian <1834638245@qq.com>
2021-02-04 14:36:57 +0800

    加入视频素材动态显示

--- module/video.js
+++ module/video.js
@@ -4,10 +4,11 @@ const { default: axios } = require('axios')
 // 视达网
 async function shida(reqData) {
   // 先查看url是否携带vid 没有则查找 有则直接用
-  // const vidSource =  await request({url:url})
-  // let vid = vidSource.match(/Params.vid = '(\S*)';/)[0].replace(/[^0-9]/ig,'')
-  const { vid } = reqData
-  vid = `vid=${vid}`
+  const { urlLink } = reqData
+  const shidaSource =  await request({url:urlLink})
+  let vid = shidaSource.match(/Params.vid = '(\S*)';/)[0].replace(/[^0-9]/ig,'')
+  let isShowDown = /素材\+源文件下载/.test(shidaSource)
+  let d = `vid=${vid}`
   // 查找视达cookie
   const result = await DB.find('cookie',{ name:'shida' })
   console.log('开始查找cookie')
@@ -19,11 +20,14 @@ async function shida(reqData) {
       const res = await request({
         url:'https://shida66.com/?c=VideoInfo&a=getNowPlayUrl',
         method: 'POST',
-        data: vid,
+        data: d,
         headers:{
           'Cookie':Cookie
         }
       })
+      // 携带上vid
+      res.vid = vid
+      res.isShowDown = isShowDown
       resolve(res)
     } catch (error) {
       reject(error)
